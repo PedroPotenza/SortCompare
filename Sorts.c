c286db87a6f11cba92a42e1bf61fe8002070fef2
Pedro Potenza <pedrohpotenza@gmail.com>
2021-12-10 00:26:08 -0300

    Feature: TrueRand and Loop Sort

--- Sorts.c
+++ Sorts.c
@@ -52,15 +52,30 @@ void FlagPrinter(){
 
 }
 
+int trueRand(){
+
+    int value;
+    value = (rand() % 32000);
+    if(value % 2 != 0)
+    {
+        int value2 = (rand() % 32000);
+        if(value % 2 != 0){
+            int value3 = (rand() % 32000);
+            return value2*value3;
+        } else {
+            value2;
+        }
+    } else {
+        return value;
+    }
+}
+
 void bestCase(int* vetor, int size){
 
     for(int i = 0; i < size; i++)
     {
         vetor[i] = i+1;
     }
-
-    printf("Concluido - Preenchimento Crescente\n");
-    flag = BEST_CASE;
 
 }
 
@@ -71,21 +86,14 @@ void worstCase(int* vetor, int size){
         vetor[i] = size - i;
     }
 
-    printf("Concluido - Preenchimento Decrescente\n");
-    flag = WORST_CASE;
-
 }
 
 void randomCase(int* vetor, int size){
 
-  
     for(int i = 0; i < size; i++)
     {
-        vetor[i] = 1 + (rand() % size);
+        vetor[i] = trueRand();
     }
-
-    printf("Concluido - Preenchimento Random\n");
-    flag = RANDOM_CASE;
     
 }
 
@@ -95,20 +103,18 @@ void showVetor(int* vetor, int size){
     }
 }
 
-void selectionSort(int* vetor, int size){
+long selectionSort(int* vetor, int size, long* numberComparisons){
 
     struct timeval start;
     gettimeofday(&start, NULL);
-    printf("Ordenando...\n");
-
-    long int numberComparisons = 0;
+    printf("Ordenando...\n\n");
 
     for (int i = 0; i < size; i++)
     {
         int indexLower = i;
         for (int j = i+1; j < size; j++)
         {
-            numberComparisons++; 
+            (*numberComparisons)++; 
             if(vetor[j] < vetor[indexLower]){ 
                 indexLower = j;
             }
@@ -121,28 +127,66 @@ void selectionSort(int* vetor, int size){
     struct timeval end;
     gettimeofday(&end, NULL);
 
-    long seconds, nseconds;
-    float time_spent;
-
-    seconds  = end.tv_sec  - start.tv_sec;
-    nseconds = end.tv_usec - start.tv_usec;
-
-    time_spent = (seconds + nseconds)/1000000.0;
-
-    printf("======= SELECTION SORT ======= \n");
-    printf("Tamanho do vetor: %d\n", size);
-    printf("Como ele estava preenchido? ");
-    FlagPrinter();
-    printf("\nTempo gasto: %f\n", time_spent);
-    printf("Numero de comparacoes: %ld\n", numberComparisons);
+    long time_spent = ((end.tv_sec * 1000000 + end.tv_usec) -(start.tv_sec * 1000000 + start.tv_usec));
+    return time_spent;
 
     flag = BEST_CASE;
+
+}
+
+void selectionSortRepeatMenu(int* vetor, int size, int countSort){
+
+    clearScreen();
+    printf("\nRepita %d vezes com qual caso inicial?\n", countSort);
+    printf("(1) - Crescente (melhor caso)\n");
+    printf("(2) - Decrescente (pior caso)\n");
+    printf("(3) - Aleatorio\n");
+
+    int option;
+    scanf("%d", &option);
+
+    long time_spent = 0, time_spend_interation;
+    long numberComparisons = 0;
+
+    for (int i = 0; i < countSort; i++)
+    {
+        if(option == 1){
+            bestCase(vetor, size);
+            time_spend_interation = selectionSort(vetor, size, &numberComparisons);
+            flag = BEST_CASE;
+        }
+
+        if(option == 2){
+            worstCase(vetor, size);
+            time_spend_interation = selectionSort(vetor, size, &numberComparisons);
+            flag = WORST_CASE;
+        }
+
+        if(option == 3){
+            randomCase(vetor, size);
+            time_spend_interation = selectionSort(vetor, size, &numberComparisons);
+            flag = RANDOM_CASE;
+        }
+
+        time_spent += time_spend_interation;
+    }
+    
+    printf("======= SELECTION SORT ======= \n");
+    printf("Quantas vezes o sort foi feito? %d\n", countSort);
+    printf("Tamanho do vetor: %d\n", size);
+    printf("Como ele estava sendo preenchido? ");
+    FlagPrinter();
+    printf("\nTempo gasto:  %lf\n", (time_spent/countSort)/1000000.0);
+    printf("Numero de comparacoes: %ld\n", numberComparisons/countSort);
+
+    flag = BEST_CASE;   
 
 }
 
 int main(void){
 
     int size; 
+    int countSorts = 10;
     int* vetor = NULL;
     srand(time(NULL)); //every execute has a diferent seed, so rand is truly random ;) 
 
@@ -162,9 +206,13 @@ int main(void){
         printf("\nFuncoes Uteis:\n");
         printf("(4) - Imprimir vetor\n");
 
-        printf("\nSorts:\n");
+        printf("\nLoop Sorts:\n");
+        printf("Atualmente cada Sort eh feito %d vezes.\n", countSorts);
         printf("(5) - Select Sort\n");
+
+        printf("(-1) - Mudar a quantidade de vezes\n");
         printf("(0) - Sair\n");
+        
 
         int option;
         printf("\nOpcao: ");
@@ -177,20 +225,33 @@ int main(void){
             sair = true;
             break;
 
+        case -1:
+            clearScreen();
+            printf("Quantas vezes cada algoritmo deve ser feito?\n");
+            scanf("%d", &countSorts);
+            clearScreen();
+            break;
+
         case 1:
             bestCase(vetor, size);
+                printf("Concluido - Preenchimento Crescente\n");
+                flag = BEST_CASE;
             pressAnything();
             clearScreen();
             break;
 
         case 2:
             worstCase(vetor, size);
+                printf("Concluido - Preenchimento Decrescente\n");
+                flag = WORST_CASE;
             pressAnything();
             clearScreen();
             break;
 
         case 3:
             randomCase(vetor, size);
+                printf("Concluido - Preenchimento Random\n");
+                flag = RANDOM_CASE;
             pressAnything();
             clearScreen();
 
@@ -203,7 +264,7 @@ int main(void){
             break;
 
         case 5:
-            selectionSort(vetor, size);
+            selectionSortRepeatMenu(vetor, size, countSorts);
             pressAnything();
             clearScreen();
             break;
